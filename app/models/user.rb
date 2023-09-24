f8e566196fb56912e4e88c2b26ba3576823041aa
Rinat273 <bairam0487@gmail.com>
2023-09-25 03:32:29 +0400

    Update test email validation should reject invalid addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save { email.downcase! }
   validates :name,  presence: true, 
                     length: { maximum: 50 }
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 }, 
                     format: { with: VALID_EMAIL_REGEX }, 
                     uniqueness: { case_sensitive: false }
@@ -12,3 +12,4 @@ class User < ApplicationRecord
 end
 
 # Завершающий commit "Make a basic User model (including secure passwords)"
+# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i # старая версия
