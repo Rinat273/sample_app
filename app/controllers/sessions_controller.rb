11a63649ddceca1481990eb0a89fa15df1568f50
Rinat273 <bairam0487@gmail.com>
2023-09-27 02:52:40 +0400

    Rendering with a flash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(email: params[:session][:email].downcase) 
     if user && user.authenticate(params[:session][:password])
       # Осуществить вход пользователя и переадресовать на страницу профиля.
     else
-      # Создать сообщение об ошибкею
-      render 'new'
+      # Сообщение об ошибке.
+      flash.now[:danger] = 'Invalid email/password combination' # Неправильно!
+      render action: 'new'
+    end
   end
-
+    
   def destroy
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,13 +5,13 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "static_pages#home"
 
-  get '/help',    to: 'static_pages#help'
-  get '/about',   to: 'static_pages#about'
-  get '/contact', to: 'static_pages#contact'
-  get '/signup',  to: 'users#new'
-  get '/login',   to: 'sessions#new'
-  get '/login',   to: 'sessions#create'
-  get '/logout',  to: 'sessions#destroy'
+  get  '/help',    to: 'static_pages#help'
+  get  '/about',   to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  get  '/login',   to: 'sessions#new'
+  post '/login',   to: 'sessions#create'
+  get  '/logout',  to: 'sessions#destroy'
   resources :users
 
 end
