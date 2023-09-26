05a1494ab68ace60d8eb62c48a8ecf2e2b40a2f7
Rinat273 <bairam0487@gmail.com>
2023-09-27 01:54:49 +0400

    Add finding and authenticating a user

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,17 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Осуществить вход пользователя и переадресовать на страницу профиля.
+    else
+      # Создать сообщение об ошибкею
+      render 'new'
+  end
+
+  def destroy
+  end
+
 end
